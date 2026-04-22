c6e21056ce6bfafbaa0405372448cea0f0be5ee7
medzie <magdawo1337@gmail.com>
2026-04-22 16:40:52 +0200

    radar status

---
+++ C_Cpp.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstdint>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+	uint8_t status = 0b10110101;// Example status byte
+
+	if (status & (1 << 0)) {
+		cout << "Power is ON." << endl;
+	}
+
+	if (status & ( 1 << 2)) {
+		cout << "Overheat detected." << endl;
+	}
+
+	status &= ~(1 << 1);
+
+	cout << bitset<8>(status) << endl;
+
+	uint8_t strength = (status >> 4);
+	cout << "signal strength: " << (int)strength << endl;
+
+
+}
